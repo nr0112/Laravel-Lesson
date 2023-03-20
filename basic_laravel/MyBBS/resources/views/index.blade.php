c0e60b388dbdee7365d255e865438b24b48cb03c
HIBIKI <hibiki.apple.1984@gmail.com>
2023-03-20 15:34:44 +0900

    Prog: Basic_Laravel - 8 #08 投稿一覧ページを作ろう

---
+++ basic_laravel/MyBBS/resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>My BBS</title>
+</head>
+<body>
+    <h1>My BBS</h1>
+    <ul>
+        <li>title</li>
+        <li>title</li>
+        <li>title</li>
+    </ul>
+</body>
+</html>
