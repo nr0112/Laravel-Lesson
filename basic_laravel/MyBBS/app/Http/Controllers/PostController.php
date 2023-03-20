ed877915809abb04b5798e19a5c5643ae3a92cf9
HIBIKI <hibiki.apple.1984@gmail.com>
2023-03-20 15:54:16 +0900

    Prog: Basic_Laravel - 14 #14 Controllerのメソッドを作ろう

--- basic_laravel/MyBBS/app/Http/Controllers/PostController.php
+++ basic_laravel/MyBBS/app/Http/Controllers/PostController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index() {
+    $posts = [
+        'Title A',
+        'Title B',
+        'Title C',
+    ];
+
+    return view('index')
+        ->with(['posts' => $posts]);
+}
 }
--- basic_laravel/MyBBS/routes/web.php
+++ basic_laravel/MyBBS/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,13 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $posts = [
-        'Title A',
-        'Title B',
-        'Title C',
-    ];
-
-    return view('index')
-        ->with(['posts' => $posts]);
-});
+Route::get('/', [PostController::class, 'index']);
