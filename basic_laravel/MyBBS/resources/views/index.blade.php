e3817ac20c21ea7e24b305cd6683fdd9b043db7d
HIBIKI <hibiki.apple.1984@gmail.com>
2023-03-20 15:56:09 +0900

    Prog: Basic_Laravel - 15 #15 詳細ページへのリンクを作ろう

--- basic_laravel/MyBBS/resources/views/index.blade.php
+++ basic_laravel/MyBBS/resources/views/index.blade.php
@@ -9,8 +9,12 @@
     <div class="container">
         <h1>My BBS</h1>
         <ul>
-            @forelse ($posts as $post)
-                <li>{{ $post }}</li>
+            @forelse ($posts as $index => $post)
+                <li>
+                    <a href="/posts/{{ $index }}">
+                        {{ $post }}
+                    </a>
+                </li>
             @empty
                 <li>No posts yet!</li>
             @endforelse
