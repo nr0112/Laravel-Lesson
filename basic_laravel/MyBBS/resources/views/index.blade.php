33b64e352733365593f068359224da9ad6409ba1
HIBIKI <hibiki.apple.1984@gmail.com>
2023-03-20 16:10:58 +0900

    Prog: Basic_Laravel - 21 #21 ページごとに内容を変えよう

--- basic_laravel/MyBBS/resources/views/index.blade.php
+++ basic_laravel/MyBBS/resources/views/index.blade.php
@@ -1,24 +1,14 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="{{ url('/css/style.css') }}">
-    <title>My BBS</title>
-</head>
-<body>
-    <div class="container">
-        <h1>My BBS</h1>
-        <ul>
-            @forelse ($posts as $index => $post)
-                <li>
-                    <a href="{{ route('posts.show', $index) }}">
-                        {{ $post }}
-                    </a>
-                </li>
-            @empty
-                <li>No posts yet!</li>
-            @endforelse
-        </ul>
-    </div>
-</body>
-</html>
+<x-layout>
+    <h1>My BBS</h1>
+    <ul>
+        @forelse ($posts as $index => $post)
+            <li>
+                <a href="{{ route('posts.show', $index) }}">
+                    {{ $post }}
+                </a>
+            </li>
+        @empty
+            <li>No posts yet!</li>
+        @endforelse
+    </ul>
+</x-layout>
--- basic_laravel/MyBBS/resources/views/posts/show.blade.php
+++ basic_laravel/MyBBS/resources/views/posts/show.blade.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="{{ url('/css/style.css') }}">
-    <title>My BBS</title>
-</head>
-
-<body>
-    <div class="container">
-        <a href="{{ route('posts.index') }}">
-            <div class="back-link">
-                &laquo Back
-            </div>
-        </a>
-        <h1>{{ $post }}</h1>
-    </div>
-</body>
-
-</html>
+<x-layout>
+    <a href="{{ route('posts.index') }}">
+        <div class="back-link">
+            &laquo Back
+        </div>
+    </a>
+    <h1>{{ $post }}</h1>
+</x-layout>
