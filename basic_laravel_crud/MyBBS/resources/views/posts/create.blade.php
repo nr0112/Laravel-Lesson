064c33a509aaac9f8699662c7cd38ee6de1fb33a
HIBIKI <hibiki.apple.1984@gmail.com>
2023-03-20 18:00:35 +0900

    Prog: Basic_Laravel_CRUD - 5 #05 フォームにCSRF対策を施そう

--- basic_laravel_crud/MyBBS/resources/views/posts/create.blade.php
+++ basic_laravel_crud/MyBBS/resources/views/posts/create.blade.php
@@ -1,9 +1,23 @@
 <x-layout>
-    <x-slot name="title"> - My BBS</x-slot>
+    <x-slot name="title">Add new post - My BBS</x-slot>
     <a href="{{ route('posts.index') }}">
         <div class="back-link">
             &laquo Back
         </div>
     </a>
-    <h1></h1>
+    <h1>Add new post</h1>
+
+    <form action="" method="post">
+        @csrf
+
+        <label>
+            Title
+            <input type="text" name="title">
+        </label>
+        <label>
+            Body
+            <textarea name="body"></textarea>
+        </label>
+        <button>Add</button>
+    </form>
 </x-layout>
