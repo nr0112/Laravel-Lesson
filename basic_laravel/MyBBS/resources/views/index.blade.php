9fd55466a7bd69a7a43ab55e60d2a6a1735041c0
HIBIKI <hibiki.apple.1984@gmail.com>
2023-03-20 15:39:33 +0900

    Prog: Basic_Laravel - 9 #09 Viewのスタイリングをしていこう

--- basic_laravel/MyBBS/resources/views/index.blade.php
+++ basic_laravel/MyBBS/resources/views/index.blade.php
@@ -2,14 +2,17 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
     <title>My BBS</title>
 </head>
 <body>
-    <h1>My BBS</h1>
-    <ul>
-        <li>title</li>
-        <li>title</li>
-        <li>title</li>
-    </ul>
+    <div class="container">
+        <h1>My BBS</h1>
+        <ul>
+            <li>title</li>
+            <li>title</li>
+            <li>title</li>
+        </ul>
+    </div>
 </body>
 </html>
