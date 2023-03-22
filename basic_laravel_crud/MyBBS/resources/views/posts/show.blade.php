bd81003bf67993a4e1b424f4c0db97004c51dc1d
HIBIKI <hibiki.apple.1984@gmail.com>
2023-03-22 17:25:09 +0900

    Prog: Basic_Laravel_CRUD - 18 #18 記事を削除する機能を実装しよう

--- basic_laravel_crud/MyBBS/resources/views/posts/show.blade.php
+++ basic_laravel_crud/MyBBS/resources/views/posts/show.blade.php
@@ -10,6 +10,10 @@
             {{ $post->title }}
         </span>
         <a href="{{ route('posts.edit', $post) }}">[Edit]</a>
+        {{-- <form action="{{ route('posts.delete', $post) }}" method="post"> --}}
+        <form action="" method="post">
+            <button class="btn">X DELETE</button>
+        </form>
     </h1>
     <p>{!! nl2br(e($post->body)) !!}</p>
 </x-layout>
