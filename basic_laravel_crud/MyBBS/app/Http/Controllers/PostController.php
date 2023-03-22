c71d82ac5e113a0cff50d351c8475203faa56011
HIBIKI <hibiki.apple.1984@gmail.com>
2023-03-22 17:15:14 +0900

    Prog: Basic_Laravel_CRUD - 15 #15 編集機能を完成させよう

--- basic_laravel_crud/MyBBS/app/Http/Controllers/PostController.php
+++ basic_laravel_crud/MyBBS/app/Http/Controllers/PostController.php
@@ -49,4 +49,22 @@ class PostController extends Controller
         return view('posts.edit')
         ->with(['post' => $post]);
     }
+
+    public function update(Request $request, Post $post)
+    {
+        $request->validate([
+            'title' => 'required|min:3',
+            'body' => 'required',
+        ], [
+            'title.required' => 'タイトルは必須です',
+            'title.min' => ':min 文字以上入力してください',
+            'body.required' => '本文は必須です',
+        ]);
+
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+
+        return redirect()->route('posts.show', $post);
+    }
 }
