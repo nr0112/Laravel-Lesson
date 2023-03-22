e9d6e5d116b9b9d6c26a7c6233dc8591073d5217
HIBIKI <hibiki.apple.1984@gmail.com>
2023-03-22 17:10:11 +0900

    Prog: Basic_Laravel_CRUD - 13 #13 編集フォームのViewを作ろう

--- basic_laravel_crud/MyBBS/resources/views/posts/edit.blade.php
+++ basic_laravel_crud/MyBBS/resources/views/posts/edit.blade.php
@@ -1,18 +1,18 @@
 <x-layout>
     <x-slot name="title">Add new post - My BBS</x-slot>
-    <a href="{{ route('posts.index') }}">
+    <a href="{{ route('posts.show', $post) }}">
         <div class="back-link">
             &laquo Back
         </div>
     </a>
-    <h1>Add new post</h1>
+    <h1>Edit post</h1>
 
-    <form action="{{ route('posts.store') }}" method="post">
+    <form action="" method="post">
         @csrf
         <div class="form-group">
             <label>
                 Title
-                <input type="text" name="title" value="{{ old('title') }}">
+                <input type="text" name="title" value="{{ old('title', $post->title) }}">
             </label>
             @error('title')
                 <div class="error">{{ $message }}</div>
@@ -21,14 +21,14 @@
         <div class="form-group">
             <label>
                 Body
-                <textarea name="body">{{ old('body') }}</textarea>
+                <textarea name="body">{{ old('body', $post->body) }}</textarea>
             </label>
             @error('body')
                 <div class="error">{{ $message }}</div>
             @enderror
         </div>
         <div class="form-button">
-            <button>Add</button>
+            <button>Update</button>
         </div>
     </form>
 </x-layout>
